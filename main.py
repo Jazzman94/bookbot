057b5f99526922f9dcc77460d07a67d296c34a7e
Jiří Jaskowiec <jirijaskowiec@seznam.cz>
2025-03-28 10:55:40 +0100

    adds: read .dcox files, single words counts

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import sys
+from docx import Document
 
-from stats import count_words, count_signle_characters, sort_by_value
+from stats import *
 
 
 def get_book_text(path_to_file: str) -> str:
     """Reads the .txt file defined by ´path_to_file´ and returns the contents as a string"""
-    with open(path_to_file) as f:
-        file_contents = f.read()
-
-    return file_contents
+    
+    if path_to_file.endswith(".docx"):
+        doc = Document(path_to_file)
+        book_text = ""
+        for para in doc.paragraphs:
+            book_text += para.text
+        return book_text
+    
+    if path_to_file.endswith(".txt"):
+        with open(path_to_file) as f:
+            book_text = f.read()
+        return book_text
+    
+    raise ValueError("File must be a .txt or .docx file")
 
 
 if __name__ == "__main__":
@@ -20,7 +31,14 @@ if __name__ == "__main__":
     path_to_file = sys.argv[1]
 
     book_text = get_book_text(path_to_file)
+
+    # Words analysis
     num_words = count_words(book_text)
+    num_count = count_single_words(book_text)
+    num_count_sorted = sort_by_value(num_count)
+
+    # Character analysis
+    total_char_count = count_total_characters(book_text)
     char_count = count_signle_characters(book_text)
     char_count_sorted = sort_by_value(char_count)
 
@@ -29,7 +47,13 @@ if __name__ == "__main__":
     print(f"Analyzing book found at {path_to_file}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
+    for word, count in num_count_sorted.items():
+        if count < 10:
+            continue
+        print(f"{word}: {count}")
+    print("----------- Total Character Count ----------")
+    print(f"Found {total_char_count} total characters")
+    print("--------- Alphabetical Character Count -------")
     for char, count in char_count_sorted.items():
         if not char.isalpha():
             continue
--- stats.py
+++ stats.py
@@ -1,15 +1,16 @@
-
+import re
+from collections import Counter
 
 def count_words(text: str) -> int:
-    """Counts the number of words in a text"""
+    """Counts the number of words in a text."""
     return len(text.split())
 
 def count_total_characters(text: str) -> int:
-    """Counts the number of characters in a text"""
+    """Counts the number of characters in a text."""
     return len(text)
 
 def count_signle_characters(text: str) -> dict[str, int]:
-    """Counts the number of single characters in a text"""
+    """Counts the number of single characters in a text."""
     char_count = {}
     for char in text.lower():
         if char in char_count:
@@ -19,6 +20,17 @@ def count_signle_characters(text: str) -> dict[str, int]:
     return char_count
 
 def sort_by_value(char_count: dict[str, int]) ->  dict[str, int]:
-    """Sorts the dictionary by value"""
+    """Sorts the dictionary by value."""
     sorted_items = sorted(char_count.items(), key=lambda x: x[1], reverse=True)
     return dict(sorted_items)
+
+def clean_text(text):
+    """Removes all non-alphanumeric characters from a text except § symbol and converts string to lowercase."""
+    text = text.lower()
+    return re.sub(r'[^\w\s§]', '', text)
+
+def count_single_words(text: str) -> dict[str, int]:
+    """Counts the number of single words in a text."""
+    text = clean_text(text)
+    return Counter(text.split())
+        
